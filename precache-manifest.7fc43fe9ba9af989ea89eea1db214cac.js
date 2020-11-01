2e62d18b87c913724c5248eaea96b07bf4f1a5f9
Olego0 <ostrovskij.013g@gmail.com>
2020-11-01 20:53:36 +0200

    Updates

---
+++ precache-manifest.7fc43fe9ba9af989ea89eea1db214cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2223bd66e21dcdce9d285521803dffff",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "44d30e35f6bb6e8d589a",
+    "url": "/goit-react-hw-02-feedback/static/css/main.da3c9807.chunk.css"
+  },
+  {
+    "revision": "3db2c03f091645567bcc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.59a998e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.59a998e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d30e35f6bb6e8d589a",
+    "url": "/goit-react-hw-02-feedback/static/js/main.ecfb8b44.chunk.js"
+  },
+  {
+    "revision": "aa1f82fba80b8c3a093b",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.09022d4a.js"
+  }
+]);
